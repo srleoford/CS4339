8ef09c2ba888bcdb9f1af33b7d4e96f03b265e9f
theophorus <srleoford@miners.utep.edu>
2022-11-11 10:35:51 -0700

    Latest in Homework #2

--- Assignment #2/mainpage.php
+++ Assignment #2/mainpage.php
@@ -1,5 +1,32 @@
 <?php
 
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'publications';
+
+$mysqli = @new mysqli(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+);
+
+if ($mysqli->connect_error) {
+    echo 'Errno: ' . $mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: ' . $mysqli->connect_error;
+    exit();
+}
+
+echo 'Success: A proper connection to MySQL was made.';
+echo '<br>';
+echo 'Host information: ' . $mysqli->host_info;
+echo '<br>';
+echo 'Protocol version: ' . $mysqli->protocol_version . "<br>";
+
+$mysqli->close();
+
 // Check if there's anyone signed in.
 // If not, print msg.
 // Else, print someone is signed in
